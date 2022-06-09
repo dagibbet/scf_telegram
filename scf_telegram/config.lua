455fd57227030e40b1bccfc570fd9310b5d366f9
dagibbet <thegibbet@hotmail.com>
2022-06-09 08:05:04 -0700

    initial conversion

---
+++ scf_telegram/config.lua
@@ -0,0 +1,41 @@
+Config = {}
+
+--LOCATIONS
+--BLIPS
+Config.postoffice = {
+    { name = "Valentine", blip = 1475382911, coords = vector3(-178.90, 626.71, 114.09), radius = 1.0 },
+    { name = "Blackwater", blip = 1475382911, coords = vector3(-875.1723, -1328.7574, 43.9580), radius = 1.0 },
+    { name = "Saint Denis", blip = 1475382911, coords = vector3(2747.2612, -1395.0283, 46.1831), radius = 1.0 },
+	
+	{name = 'Rhodes',           blip = 1475382911,          coords = vector3(1225.57, -1293.87, 76.9),   radius = 1.0},
+    {name = 'Wallace',          blip = 1475382911,          coords = vector3(-1300.84, 399.29, 95.42),   radius = 1.0},
+    {name = 'Riggs',            blip = 1475382911,          coords = vector3(-1095.31, -576.94, 82.4),   radius = 1.0},
+    {name = 'Van Horn Trading',  blip = 1475382911,          coords = vector3(2986.42, 569.03, 44.67),   radius = 1.0},
+    {name = 'Annesburg',         blip = 1475382911,          coords = vector3(2939.29, 1288.23, 44.65),   radius = 1.0},
+    {name = 'Strawberry',        blip = 1475382911,      	coords = vector3(-1764.93, -384.45, 157.74), radius = 1.0},
+    {name = 'Armadillo',         blip = 1475382911,          coords = vector3(-3733.68, -2597.89, -12.94),radius = 1.0},
+	{name = 'Emerald Station',         blip = 1475382911,          coords = vector3(1521.97, 440.03, 90.73),radius = 1.0},
+}
+
+Config.anonpostoffice = {
+	{ name = "Valentine Sheriff Dept", coords = vector3(-278.72, 803.63, 119.43), radius = 1.0 },
+	{ name = "Rhodes Sheriff Dept", coords = vector3(1362.03, -1303.56, 77.75), radius = 1.0 },
+	{ name = "Annesburg Sheriff Dept", coords = vector3(-1812.57, -353.80, 165.33), radius = 1.0 },
+	{ name = "Blackwater Sheriff Dept", coords = vector3(-762.65, -1271.98, 43.84), radius = 1.0 },
+	{ name = "Armadillo Sheriff Dept", coords = vector3(-3624.31, -2601.64, -12.52), radius = 1.0 },
+	{ name = "Tumbleweed Sheriff Dept", coords = vector3(-5530.41, -2929.62, -1.58), radius = 1.0 },
+	{ name = "Saint Denis Sheriff Dept", coords = vector3(2507.48, -1300.98, 48.77), radius = 1.0 },
+}
+
+--TRANSLATE
+Config.post = "Post Office"
+Config.OpenPost = "press"
+Config.pdpost = "Tip Line"
+
+--OPEN KEY
+Config.keys = {
+    G = 0x760A9C6F
+}
+
+-- CHARGE PER TELEGRAM SENT
+Config.Pay = 0.50 --cents
---
+++ scf_telegram/server.lua
@@ -0,0 +1,146 @@
+
+
+function getRandomPostbox()
+    local chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    local length = 2
+    local randomString = ''
+
+    math.randomseed(os.time())
+
+    charTable = {}
+    for c in chars:gmatch "." do
+        table.insert(charTable, c)
+    end
+
+    for i = 1, length do
+        randomString = randomString .. charTable[math.random(1, #charTable)]
+    end
+    return randomString .. math.random(1000, 9999)
+end
+
+function getPostbox(charid, cb)
+		result = MySQL.Sync.fetchAll('SELECT * FROM players WHERE citizenid = @chid', { ['@chid'] = charid }) --, function(result)	
+        if result ~= nil then			
+            cb(result[1]['postbox'])
+        end
+        return nil
+    --end)
+end
+
+function getOrGenerate(charid, cb)
+    getPostbox(charid, function(s)		
+        local postbox = s
+        if postbox == nil then
+            postbox = getRandomPostbox()
+            MySQL.Async.execute('UPDATE players SET postbox = @pox WHERE citizenid = @chid', { ['@chid'] = charid, ['@pox'] = postbox }) --, function() cb(postbox) end)
+			cb(postbox)
+        else
+            cb(postbox)
+        end
+    end)
+end
+
+RegisterServerEvent("scf_telegram:check_inbox")
+AddEventHandler("scf_telegram:check_inbox", function()
+    local _source = source
+    local User = exports['qbr-core']:GetPlayer(_source)
+    local Character = User.PlayerData
+    getOrGenerate(Character.citizenid, function(postbox)
+        result = MySQL.Sync.fetchAll('SELECT * FROM telegrams WHERE recipient = @reci ORDER BY id DESC', { ['@reci'] = postbox }) --, function(result)
+			local res = {}
+            res['box'] = postbox
+            res['firstname'] = Character.name
+            res['list'] = result
+            if result ~= nil then
+                TriggerClientEvent("inboxlist", _source, res)
+            end
+        --end)
+    end)
+end)
+
+RegisterServerEvent("scf_telegram:check_inboxPD")
+AddEventHandler("scf_telegram:check_inboxPD", function()
+    local _source = source
+	result = MySQL.Sync.fetchAll('SELECT * FROM telegrams WHERE recipient = @reci ORDER BY id DESC', { ['@reci'] = "anon" })--, function(result)
+		local res = {}
+		res['box'] = "ANON"
+		res['firstname'] = "ANON"			
+		res['list'] = result
+		if result ~= nil then
+		print("here")
+			TriggerClientEvent("inboxlist", _source, res)
+		end
+	--end)
+end)
+
+RegisterServerEvent("scf_telegram:SendTelegram")
+AddEventHandler("scf_telegram:SendTelegram", function(data)
+    local _source = source
+    local User = exports['qbr-core']:GetPlayer(_source)
+    local Character = User.PlayerData
+    local currentMoney = User.Functions.GetMoney('cash')
+    local removeMoney = Config.Pay
+
+    if currentMoney >= removeMoney then
+        getOrGenerate(Character.citizenid, function(postbox)
+            local sentDate = os.date("%x")
+            result = MySQL.Sync.fetchAll('SELECT postbox FROM players WHERE postbox = @pox',{['@pox'] = data.recipient}) --, function(result)
+                if result[1] ~= nil then
+                    if data.recipient == nil or data.recipient == '' and data.subject == nil or data.subject == '' then
+                        TriggerClientEvent('QBCore:Notify', _source, 9,  "You need to add a PO Box and the Subject of your message", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE') 
+                    else
+                        local Parameters = { ['recipient'] = data.recipient, ['sender'] = data.sender, ['subject'] = data.subject, ['sentTime'] = sentDate, ['message'] = data.message, ['postoffice'] = data.postoffice }
+                        MySQL.Async.execute("INSERT INTO telegrams ( `recipient`,`sender`,`subject`,`sentTime`,`message`,`postoffice`) VALUES ( @recipient,@sender, @subject,@sentTime,@message,@postoffice )", Parameters)
+                        User.Functions.RemoveMoney("cash", removeMoney)
+                        TriggerClientEvent('QBCore:Notify', _source, 9,  "Telegram has been sent for an ammount of " .. removeMoney .. "cents", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+                    end
+                elseif data.recipient == "ANON" or data.recipient == "anon" then
+					local Parameters = { ['recipient'] = data.recipient, ['sender'] = data.sender, ['subject'] = data.subject, ['sentTime'] = sentDate, ['message'] = data.message, ['postoffice'] = data.postoffice }            
+					MySQL.Async.execute("INSERT INTO telegrams ( `recipient`,`sender`,`subject`,`sentTime`,`message`,`postoffice`) VALUES ( 'anon','anon', @subject,@sentTime,@message,@postoffice )", Parameters)
+                    TriggerClientEvent('QBCore:Notify', _source, 9,  "Telegram has been sent free of charge.", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+				else
+                    TriggerClientEvent('QBCore:Notify', _source, 9,  "The POBox you are trying to send the Telegram to, does not exist", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')  
+                end
+            --end)
+        end)
+    else
+        TriggerClientEvent('QBCore:Notify', _source, 9,  "you do not have enough money", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')  
+    end
+end)
+
+RegisterServerEvent("scf_telegram:getTelegram")
+AddEventHandler("scf_telegram:getTelegram", function(tid)
+    local _source = source
+    local User = exports['qbr-core']:GetPlayer(_source)
+    local telegram = {}
+    Citizen.Wait(0)
+    result = MySQL.Sync.fetchAll('SELECT * FROM telegrams WHERE id = @id', { ['@id'] = tid }) --, function(result)
+
+        if result[1] ~= nil then
+            telegram['recipient'] = User.PlayerData.firstname
+            telegram['sender'] = result[1]['sender']
+            telegram['sentTime'] = result[1]['sentTime']
+            telegram['subject'] = result[1]['subject']
+            telegram['message'] = result[1]['message']
+            MySQL.Async.execute('UPDATE telegrams SET status = 1 WHERE id = @id', { ['@id'] = tid })
+            TriggerClientEvent("messageData", _source, telegram)
+        end
+    --end)
+end)
+
+RegisterServerEvent("scf_telegram:DeleteTelegram")
+AddEventHandler("scf_telegram:DeleteTelegram", function(tid)
+	local _source = source
+
+    Citizen.Wait(0)
+   
+    result = MySQL.Sync.fetchAll("SELECT * FROM telegrams WHERE id = @id", { ['@id'] = tid }) --, function(result)
+        if result[1] ~= nil then
+            MySQL.Async.execute("DELETE FROM telegrams WHERE id = @id", { ["@id"] = tid })
+            TriggerClientEvent('QBCore:Notify', _source, 9,  "Telegram deleted.", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')  
+        else
+            TriggerClientEvent('QBCore:Notify', _source, 9,  "Failed to delete your message.", 3000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')  
+        end
+    --end)
+    
+end)
